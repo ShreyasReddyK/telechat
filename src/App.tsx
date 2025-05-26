d8df647a5f7b72aae87dc0f2a8cb539977f355a1
Shreyas <43809072+ShreyasReddyK@users.noreply.github.com>
2025-05-27 04:59:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className="bg-gradient-to-br from-red-600 to-purple-600 text-white p-6 h-screen justify-center flex items-center">
 			<ChatApp />
-			<p className="text-purple-400 fixed bottom-1 right-1 select-none">
+			<p className="text-purple-300 fixed bottom-1 right-1 select-none">
 				By Shreyas for Teleparty.
 			</p>
 		</div>
